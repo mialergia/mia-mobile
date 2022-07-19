a91cf45a99f313d5e0d714fa529a129fb0042372
Tamara Suarez <tamarasuarez@Tamaras-MacBook-Pro.local>
2022-07-19 20:59:47 +0100

    fix: arm64

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: [
+    [
+      'module:metro-react-native-babel-preset',
+      {
+        unstable_disableES6Transforms: true,
+      },
+    ],
+  ],
   plugins: [
     [
       'module-resolver',
---
+++ ios/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  MIA
+//
+//  Created by Tamara Suarez on 20/07/2022.
+//  Copyright © 2022 Facebook. All rights reserved.
+//
+
+import Foundation
